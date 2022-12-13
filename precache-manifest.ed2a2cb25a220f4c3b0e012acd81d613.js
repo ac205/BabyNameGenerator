0db3d7d654fc2f91b8da8f9961855be608623def
UFC205 <ufc205@gmail.com>
2022-12-13 13:04:56 -0400

    Updates

---
+++ precache-manifest.ed2a2cb25a220f4c3b0e012acd81d613.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb1dcbb2c3388ccf199ad2340b322e8",
+    "url": "/BabyNameGenerator/index.html"
+  },
+  {
+    "revision": "793787e884e444b591cb",
+    "url": "/BabyNameGenerator/static/js/2.fd5f6855.chunk.js"
+  },
+  {
+    "revision": "7b1771267feb0da180fa4afcb5df9175",
+    "url": "/BabyNameGenerator/static/js/2.fd5f6855.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ac6bc39e98d54f95ea2",
+    "url": "/BabyNameGenerator/static/js/main.92f1b031.chunk.js"
+  },
+  {
+    "revision": "f44c6dca0d75be482f80",
+    "url": "/BabyNameGenerator/static/js/runtime-main.8b0abe0d.js"
+  }
+]);
